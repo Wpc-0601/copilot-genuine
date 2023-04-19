9def6800d87da4a1ce14baca1f33b856f391d808
Ame <wangpengchao@wangpengchaodeMacBook-Pro.local>
2023-04-19 17:40:17 +0800

    delete debug log

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/Wpc-0601/copilot-genuine/config/bootstrap"
 	"github.com/Wpc-0601/copilot-genuine/config/global"
 	"github.com/gin-gonic/gin"
@@ -14,9 +13,5 @@ func main() {
 	engine.GET("/ping", func(context *gin.Context) {
 		context.String(http.StatusOK, "This is an new Application...")
 	})
-	fmt.Println("port is :", global.App.Configuration.App.Port)
-	fmt.Println("env is: ", global.App.Configuration.App.Env)
-	fmt.Println("appName is: ", global.App.Configuration.App.AppName)
-	fmt.Println("appUrl is: ", global.App.Configuration.App.AppUrl)
 	_ = engine.Run(":" + global.App.Configuration.App.Port)
 }
